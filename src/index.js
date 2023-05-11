15f6d8459f6e36533c8d1de4187e64128a586fb8
Hrustch <nik.stalker47@gmail.com>
2023-05-11 13:53:21 +0300

    enhanced update

--- src/index.js
+++ src/index.js
@@ -10,26 +10,57 @@ const loader = document.querySelector(".end-collection-text");
 export const form = document.querySelector("#search-form");
 const lightbox = new SimpleLightbox(".gallery a");
 
-async function sendQuery(e) {
+
+
+function submit(event) {
+    if (!form.elements.searchQuery.value.trim()) {
+        return;
+    }
+    console.log("submit is runing")
+    event.preventDefault();
+    page = 1
+    gallery.innerHTML = ""
+    sendQuery()
+}
+
+
+
+
+
+
+
+
+
+async function sendQuery() {
     try {
         if (!form.elements.searchQuery.value.trim()) {
             return;
         }
 
-        e.preventDefault();
-        gallery.innerHTML = ""
-
         const response = await callForImgs(page)
-        await renderImages(response.data.hits)
+        
+        console.log(`Кількість відповідей: ${response.data.hits.length}`)
+
+
         if (response.data.totalHits === 0) {
             Notiflix.Notify.failure("Sorry, we didn't find any images for your request")
+            console.log("Відповіді нема, нічого не робим")
         }
         if (response.data.totalHits === 1) {
+            await renderImages(response.data.hits)
             Notiflix.Notify.success(`Hooray! We found 1 image.`)
         }
-        if (response.data.totalHits > 1) {
+        if (response.data.totalHits > 1 && response.data.totalHits < 40) {
+            await renderImages(response.data.hits)
             Notiflix.Notify.success(`Hooray! We found ${response.data.totalHits} images.`)
         }
+        if (response.data.totalHits > 40) {
+            await renderImages(response.data.hits)
+            loader.classList.add("visible")
+            console.log("Лоадер видимий")
+            Notiflix.Notify.success(`Hooray! We found ${response.data.totalHits} images.`)
+        }
+        return response;
 
     } catch {
 
@@ -58,22 +89,22 @@ function renderImages(images) {
             <img class="gallery-item_img" src="${webformatURL}" alt="${tags}" loading="lazy" />
             <div class="info">
             <p class="info-item"><b>Likes</b>${likes}</p>
-                        <p class="info-item"><b>Views</b>${views}</p>
-                        <p class="info-item"><b>Comments</b>${comments}</p>
-                        <p class="info-item"><b>Downloads</b>${downloads}</p>
-                        </div>
-                        </div>
-                        </a>
-                        `;
+            <p class="info-item"><b>Views</b>${views}</p>
+            <p class="info-item"><b>Comments</b>${comments}</p>
+            <p class="info-item"><b>Downloads</b>${downloads}</p>
+            </div>
+            </div>
+            </a>
+            `;
         })
         .join('');
 
     gallery.insertAdjacentHTML('beforeend', markup);
     lightbox.refresh()
-    loader.classList.add("visible")
+    
 }
 
-form.addEventListener("submit", sendQuery)
+form.addEventListener("submit", submit)
 
 
 
@@ -81,11 +112,16 @@ async function loadMorePhotos(entries, observer) {
     entries.forEach(async (entry) => {
         if (entry.isIntersecting) {
             page++;
-            const response = await callForImgs(page);
-            const images = response.data.hits;
+            console.log("Дозавантажити картинки!")
+
             loader.classList.remove("visible")
+            console.log("Лоадер невидимий")
+
+            const response = await sendQuery();
+            const images = response.data.hits;
+            console.log(`Картинок: ${images.length}`)
+            
             if (images.length === 0) {
-                observer.unobserve(loader);
                 Notiflix.Notify.info(`We're sorry, but you've reached the end of search results.`)
                 return;
             }
